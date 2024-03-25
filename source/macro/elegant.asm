953219ed9961be017ef4fe47ba806aaf3591fb58
Rui Costa <rui.fajozes@gmail.com>
2024-03-25 16:24:30 +0000

    d

--- source/macro/elegant.asm
+++ source/macro/elegant.asm
@@ -47,3 +47,84 @@ DLOOP_5 =  0
     ENDM
 
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; 
+; IF 
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+IF_COUNT = 0;
+
+
+    MACRO IF_PUSH 
+IF_COUNT = IF_COUNT +1 
+    ENDM
+
+    MACRO IF_POP
+IF_COUNT = IF_COUNT - 1 
+    ENDM
+ 
+
+    MACRO _if_not arg
+
+    IF_PUSH 
+
+    IF IF_COUNT = 1 
+        jr arg, 1F
+    ENDIF
+
+    IF IF_COUNT = 2
+        jr arg, 2F
+    ENDIF
+
+    IF IF_COUNT = 3
+        jr arg, 3F
+    ENDIF
+
+    IF IF_COUNT = 4
+        jr arg, 4F
+    ENDIF
+
+    IF IF_COUNT = 5 
+        jr arg, 5F
+    ENDIF
+
+
+
+
+    ENDM
+
+    MACRO _end_if
+
+
+ 
+
+    IF IF_COUNT = 1 
+1
+    ENDIF
+
+    IF IF_COUNT = 2
+2
+    ENDIF
+
+    IF IF_COUNT = 3
+3
+    ENDIF
+
+    IF IF_COUNT = 4
+4
+    ENDIF
+
+    IF IF_COUNT = 5 
+5
+    ENDIF
+
+
+
+
+
+        IF_POP
+    ENDM
+
+
+
